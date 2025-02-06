b3a1569c189e036d9ea897f8d0483603775395da
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-06 14:50:02 -0300

    chore: initial project setup.

---
+++ src/main/java/com/example/exercicio_sql_01/ExercicioSql01Application.java
@@ -0,0 +1,13 @@
+package com.example.exercicio_sql_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioSql01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioSql01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exercicio_sql_01/ExercicioSql01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicio_sql_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioSql01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
